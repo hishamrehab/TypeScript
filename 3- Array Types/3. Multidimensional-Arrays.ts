abb55fc54f7f1ea10895dd174e8ebfb1869ec3f3
hishamrehab <hishamrehab30@gmail.com>
2026-01-02 18:33:42 +0200

    tripleDimentionalArrays

---
+++ 3- Array Types/3. Multidimensional-Arrays.ts
@@ -0,0 +1,9 @@
+const singleDimentionalArray  : number[] = [1 , 2 , 3 , 4 ,5 ];
+const multiDimentaioalArray : number[] [] = [[1, 2 , 3 ,4 , 5]];
+const tripleDimentionalArray : number [] [] []  = [[[ 1 ,2 , 3 , 4 , 5]]];
+
+console.log(singleDimentionalArray);
+console.log(multiDimentaioalArray);
+console.log(tripleDimentionalArray);
+
+
