95d2b0e4beac9f95c43a0a1e0a1949e7cb67b2e5
hishamrehab <hishamrehab30@gmail.com>
2026-01-02 15:38:58 +0200

    Any Type

--- 2-Inferring.ts
+++ 2-Inferring.ts
@@ -1,8 +1,8 @@
 // tech = 12;
-// let favNumber = 8;        // error 
-favNumber = "something";
+let favNumber = 8;        // error 
+// favNumber = "something";
 let tech = "Typescript";
-let tsHard = true;
+// let tsHard = true;
 
 console.log(typeof favNumber);
 console.log(typeof tsHard);
---
+++ 3-AnyType.ts
@@ -0,0 +1,6 @@
+let color : any = "crimson";
+
+color = 20;
+color = true;
+
+console.log(color);
