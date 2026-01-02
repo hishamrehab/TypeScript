a82d961b1ceba5e17b3f74f3f904ae4a865865f2
hishamrehab <hishamrehab30@gmail.com>
2026-01-02 18:28:50 +0200

    Alternate-Syntax

--- 3- Array Types/2. Alternate-Syntax.ts
+++ 3- Array Types/2. Alternate-Syntax.ts
@@ -1,3 +1,7 @@
-const items : Array<string> = []
+const items : Array<number> = [1 , 4 , 5];
 
 console.log(items)
+
+
+const itemList : Array<number> = ["something" , true , {}]; //error syntax
+console.log("itemList", itemList);
