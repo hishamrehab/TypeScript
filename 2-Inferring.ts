e1150fe5f3118de861b359c83796068c1a99eade
hishamrehab <hishamrehab30@gmail.com>
2026-01-02 15:28:19 +0200

    Type Inferring

---
+++ 2-Inferring.ts
@@ -0,0 +1,9 @@
+// tech = 12;
+// let favNumber = 8;        // error 
+favNumber = "something";
+let tech = "Typescript";
+let tsHard = true;
+
+console.log(typeof favNumber);
+console.log(typeof tsHard);
+console.log(typeof tech);
