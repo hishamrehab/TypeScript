a49d621ebedc4b0ad117e497af78138dad82aa07
hishamrehab <hishamrehab30@gmail.com>
2026-01-03 01:23:33 +0200

    Tuples

---
+++ 6. Tuples and Enums/1. Tuples.ts
@@ -0,0 +1,15 @@
+let myTuple: [number  , string];
+myTuple = [10 , "Hello World"];
+// myTuple = [ "Hello World" , 10 ,]; //error 
+console.log("myTuple" , myTuple);
+// -------------------------------
+
+
+const products :(number | string)[] = ["item1" , 12]
+console.log(products)
+
+
+//------------------------
+const games:  [string , string , string] = ["Game 1" , "Game 2" , "Game 3"];
+const memeories:  [string , number , boolean ] = ["Game 1" , 2 , true];
+console.log(games);
