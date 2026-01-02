7d99e00250a81939508240de842cee0940b672fd
hishamrehab <hishamrehab30@gmail.com>
2026-01-02 15:22:12 +0200

    Type Annotation

---
+++ 1-Annotation.ts
@@ -0,0 +1,18 @@
+// Annotation
+
+// Strings 
+let myName : string  = "hisham"
+myName = "Some oher person"
+console.log(myName);
+
+
+// Numbers 
+let favNumber : number = 12;
+console.log(favNumber);
+
+// Boolean
+let tsHard : boolean = false;
+tsHard = true;
+console.log(tsHard);
+
+
