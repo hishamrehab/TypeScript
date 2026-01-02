2fd798722441c6966516292590ab01596790bbfb
hishamrehab <hishamrehab30@gmail.com>
2026-01-02 15:06:58 +0200

    Annotation

---
+++ index.ts
@@ -0,0 +1,21 @@
+type Person= {
+    name :string;
+};
+
+const person :Person = {
+    name: "John"
+}
+
+console.log(person.name);
+
+// Annotation
+
+// ket myVar = value;
+
+let myName:string = "Huxn WebDev"
+
+myName = "Another Name";
+console.log(myName);
+
+// Error 
+myName = 12;
