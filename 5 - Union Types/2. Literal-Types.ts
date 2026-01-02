5de86d10acd6838a157c89ee24fb170d633261c9
hishamrehab <hishamrehab30@gmail.com>
2026-01-03 01:01:24 +0200

    Literal Types

---
+++ 5 - Union Types/2. Literal-Types.ts
@@ -0,0 +1,20 @@
+//--------------------------------------
+let color : "red" | "crimson" | "blue";
+color = "red"
+console.log(color);
+
+// ---------------------------------
+
+//--------------------------------
+let isTrue: true | false;
+isTrue = true;
+console.log(isTrue);
+// ------------------------------------
+
+let password : "secretpassword" = "secretpassword"
+console.log(password);
+
+// ---------------------------------------
+
+
+
