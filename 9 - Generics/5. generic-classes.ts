7544bdcb073dc47524c1fecbee1a3d1fd0f94343
hishamrehab <hishamrehab30@gmail.com>
2026-01-03 21:00:35 +0200

    Generic Classes

---
+++ 9 - Generics/5. generic-classes.ts
@@ -0,0 +1,28 @@
+class Box<T> {
+    private content :T;
+
+    constructor(initialContent : T) {
+     this.content = initialContent;
+    }
+
+    getContent() :T {
+        return this.content;
+    }
+    setContent(newContent :T ) : void {
+       this.content = newContent
+    }
+} 
+
+
+const stringBox =  new Box<string>("Hello");
+stringBox.setContent("New Content Added");
+console.log("stringBox" ,stringBox.getContent());
+
+
+
+
+const numberBox = new Box<number>(20)
+console.log(numberBox.getContent());
+numberBox.setContent(100)
+console.log(numberBox.getContent());
+
