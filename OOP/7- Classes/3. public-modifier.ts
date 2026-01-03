0611b85ae94f8464e700db3f2156f6c89c7ef2d2
hishamrehab <hishamrehab30@gmail.com>
2026-01-03 02:21:59 +0200

    private modifier

--- OOP/7- Classes/3. public-modifier.ts
+++ OOP/7- Classes/3. public-modifier.ts
@@ -1,13 +1,13 @@
-class Person {
-    public first : string;
-    public last : string;
+class Player {
+  public readonly first: string;
+  public readonly last: string;
+  public score: number = 0;
 
-    constructor(first : string , last : string ) {
-        this.first = first;
-        this.last = last;
-    }
+  constructor(first: string, last: string) {
+    this.first = first;
+    this.last = last;
+  }
 }
 
-let p1 = new Person("John" , "Deo");
-console.log(p1.first);
-console.log(p1.last);
+const dog1 = new Player("Elton", "Steele");
+console.log(dog1.score);
---
+++ OOP/7- Classes/4. private-modifier.ts
@@ -0,0 +1,15 @@
+class Person {
+    private first : string;
+    public last : string;
+    constructor (first : string , last : string) {
+        this.first = first;
+        this.last = last;
+    }
+
+    getName() : string {
+        return `${this.first} ${this.last}`
+    }
+}
+
+let p1 = new Person("John" , "Doe");
+console.log(p1.getName());
